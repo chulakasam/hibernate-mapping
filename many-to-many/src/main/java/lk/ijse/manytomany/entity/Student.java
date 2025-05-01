75ab402fa71b46e3e1c73b9a4099d692dc50497e
chulakasam <sampathchulaka33872@gmail.com>
2025-05-01 17:04:27 +0530

    many to many create student entity

--- many-to-many/src/main/java/lk/ijse/manytomany/entity/Student.java
+++ many-to-many/src/main/java/lk/ijse/manytomany/entity/Student.java
@@ -7,15 +7,19 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "student")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+
     @Column(name = "first_name")
     private String firstname;
+
     @Column(name = "last_name")
     private String lastname;
+
     @Column(name = "email")
     private String email;
 
